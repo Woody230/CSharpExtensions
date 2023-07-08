7d0f27a299262125da3e6570b12cc7c8e1d78f82
Woody230 <bselzer1@outlook.com>
2023-07-08 12:21:39 -0400

    add friendly validator

---
+++ FluentValidation/Library/Validators/FriendlyValidator.cs
@@ -0,0 +1,36 @@
+﻿using FluentValidation;
+using FluentValidation.Results;
+
+namespace Woody230.FluentValidation.Validators
+{
+    /// <summary>
+    /// <para>
+    /// Validates a <typeparamref name="T"/> in a non-exception based way.
+    /// </para>
+    /// <para>
+    /// When the model is null, then a validation failure is added to the result's errors.
+    /// This is in constrast to the default behavior of throwing an exception.
+    /// </para>
+    /// </summary>
+    /// <typeparam name="T">The type of model.</typeparam>
+    public abstract class FriendlyValidator<T>: AbstractValidator<T>
+    {
+        /// <summary>
+        /// When the model is null, then a validation failure is added to the result's errors.
+        /// This is in constrast to the default behavior of throwing an exception.
+        /// </summary>
+        /// <param name="context">The context.</param>
+        /// <param name="result">The result.</param>
+        /// <returns>True if valid, otherwise false.</returns>
+        protected override bool PreValidate(ValidationContext<T> context, ValidationResult result)
+        {
+            if (context.InstanceToValidate == null)
+            {
+                result.Errors.Add(new ValidationFailure("", "Model is not provided."));
+                return false;
+            }
+
+            return base.PreValidate(context, result);
+        }
+    }
+}
