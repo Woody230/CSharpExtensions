5f3c4ee7d77338bd04fe9cd0b8490355e73a7a95
Woody230 <bselzer1@outlook.com>
2023-07-09 07:22:01 -0400

    use friendly for model validator

--- FluentValidation/Web/Validators/EventValidator.cs
+++ FluentValidation/Web/Validators/EventValidator.cs
@@ -5,7 +5,7 @@ using Woody230.FluentValidation.Web.Models;
 namespace Woody230.FluentValidation.Web.Validators;
 
 /// <inheritdoc/>
-public class EventValidator: OptionalValidator<Event>
+public class EventValidator: FriendlyValidator<Event>
 {
     /// <inheritdoc/>
     public EventValidator()
