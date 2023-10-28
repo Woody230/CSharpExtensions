075ba2081f480c87dd6b8dacf09fc1d16d688a2c
Woody230 <bselzer1@outlook.com>
2023-10-28 12:28:09 -0400

    app module

---
+++ AspNetCore.App/Library/Modules/IWebApplicationModule.cs
@@ -0,0 +1,14 @@
+﻿using Woody230.AspNetCore.App.Builder;
+
+namespace Woody230.AspNetCore.App.Modules;
+
+/// <summary>
+/// Represents a module that can apply settings to the web application.
+/// </summary>
+public interface IWebApplicationModule
+{
+    /// <summary>
+    /// Applies settings to the web application.
+    /// </summary>
+    public void Apply(IWebApplication application);
+}
