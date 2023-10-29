7cb1701afc7ba82279ee6cbfe992ea64433b1325
Woody230 <bselzer1@outlook.com>
2023-10-29 15:30:48 -0400

    logging runner

---
+++ AspNetCore/Library/Application/Runner/LoggingWebApplicationRunner.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Threading.Tasks;
+
+namespace Woody230.AspNetCore.Application.Runner;
+
+/// <summary>
+/// Decorates a <see cref="IWebApplicationRunner"/> with the ability to catch and log exceptions, and rethrows the exception.
+/// </summary>
+public sealed class LoggingWebApplicationRunner : IWebApplicationRunner
+{
+    private readonly IWebApplicationRunner _applicationRunner;
+    private readonly ILogger<IWebApplicationRunner> _logger;
+
+    public LoggingWebApplicationRunner(IWebApplicationRunner applicationRunner, ILogger<IWebApplicationRunner> logger)
+    {
+        _applicationRunner = applicationRunner;
+        _logger = logger;
+    }
+
+    public void Run(IWebApplication application)
+    {
+        try
+        {
+            _applicationRunner.Run(application);
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "Failed to run the application.");
+            throw;
+        }
+    }
+
+    public Task RunAsync(IWebApplication application)
+    {
+        try
+        {
+            return _applicationRunner.RunAsync(application);
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "Failed to run the application.");
+            throw;
+        }
+    }
+}
