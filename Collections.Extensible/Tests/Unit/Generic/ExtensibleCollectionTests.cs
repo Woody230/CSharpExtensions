7ba9ef2d824d67372bfe4a69a1cd66e91b611971
Woody230 <bselzer1@outlook.com>
2023-11-05 11:48:21 -0500

    extensible collection +/- tests

---
+++ Collections.Extensible/Tests/Unit/Generic/ExtensibleCollectionTests.cs
@@ -0,0 +1,67 @@
+﻿using FluentAssertions;
+using Woody230.Collections.Extensible.Generic;
+using Xunit;
+
+namespace Woody230.Collections.Extensible.Tests.Unit.Generic;
+public class ExtensibleCollectionTests
+{
+    [Fact]
+    public void Add()
+    {
+        // Arrange
+        IExtensibleCollection<int> first = new ExtendedList<int>() { 5, 8, 13 };
+        IExtensibleCollection<int> second = new ExtendedSet<int>() { 9, 13, 17 };
+
+        // Act
+        IExtensibleCollection<int> merged = first + second;
+
+        // Assert
+        merged.Should().BeSameAs(first);
+        merged.Should().BeEquivalentTo(new ExtendedList<int>() { 5, 8, 13, 9, 13, 17 });
+    }
+
+    [Fact]
+    public void AddReverse()
+    {
+        // Arrange
+        IExtensibleCollection<int> first = new ExtendedSet<int>() { 9, 13, 17 };
+        IExtensibleCollection<int> second = new ExtendedList<int>() { 5, 8, 13 };
+
+        // Act
+        IExtensibleCollection<int> merged = first + second;
+
+        // Assert
+        merged.Should().BeSameAs(first);
+        merged.Should().BeEquivalentTo(new ExtendedSet<int>() { 5, 8, 13, 9, 17 });
+    }
+
+    [Fact]
+    public void Subtract()
+    {
+        // Arrange
+        IExtensibleCollection<int> first = new ExtendedList<int>() { 5, 8, 13 };
+        IExtensibleCollection<int> second = new ExtendedSet<int>() { 9, 13, 17 };
+
+        // Act
+        IExtensibleCollection<int> merged = first - second;
+
+        // Assert
+        merged.Should().BeSameAs(first);
+        merged.Should().BeEquivalentTo(new ExtendedList<int>() { 5, 8 });
+    }
+
+    [Fact]
+    public void SubtractReverse()
+    {
+        // Arrange
+        IExtensibleCollection<int> first = new ExtendedSet<int>() { 9, 13, 17 };
+        IExtensibleCollection<int> second = new ExtendedList<int>() { 5, 8, 13 };
+
+        // Act
+        IExtensibleCollection<int> merged = first - second;
+
+        // Assert
+        merged.Should().BeSameAs(first);
+        merged.Should().BeEquivalentTo(new ExtendedSet<int>() { 9, 17 });
+    }
+}
