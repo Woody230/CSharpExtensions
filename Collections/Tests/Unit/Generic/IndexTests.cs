2a7c4622c8a447aaf7638d49d30ac9036bdadbb0
Woody230 <bselzer1@outlook.com>
2023-11-05 06:33:23 -0500

    with index test

---
+++ Collections/Tests/Unit/Generic/IndexTests.cs
@@ -0,0 +1,26 @@
+﻿using FluentAssertions;
+using System.Collections.Generic;
+using Woody230.Collections.Generic;
+using Xunit;
+
+namespace Woody230.Collections.Tests.Unit.Generic;
+public class IndexTests
+{
+    [Fact]
+    public void WithIndex()
+    {
+        // Arrange
+        var list = new List<int>() { 5, 9, 17 };
+
+        // Act
+        IEnumerable<(int, int)> withIndex = list.WithIndex();
+
+        // Assert
+        withIndex.Should().BeEquivalentTo(new List<(int, int)>()
+        {
+            (0, 5),
+            (1, 9),
+            (2, 17)
+        });
+    }
+}
