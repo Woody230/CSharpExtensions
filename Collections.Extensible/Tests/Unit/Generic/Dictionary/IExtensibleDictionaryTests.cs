f12f0b9bac615e19525442e2d7016e4ee38f9320
Woody230 <bselzer1@outlook.com>
2023-11-05 12:15:44 -0500

    extensible dictionary tests

---
+++ Collections.Extensible/Tests/Unit/Generic/Dictionary/IExtensibleDictionaryTests.cs
@@ -0,0 +1,138 @@
+﻿using FluentAssertions;
+using Woody230.Collections.Extensible.Generic;
+using Xunit;
+
+namespace Woody230.Collections.Extensible.Tests.Unit.Generic.Dictionary;
+public class IExtensibleDictionaryTests
+{
+    [Fact]
+    public void Add()
+    {
+        // Arrange
+        IExtensibleDictionary<string, int> first = new ExtendedDictionary<string, int>() 
+        {
+            ["Foo"] = 5,
+            ["Bar"] = 8,
+            ["Baz"] = 13
+        };
+        IExtensibleDictionary<string, int> second = new ExtendedDictionary<string, int>() 
+        {
+            ["Fizz"] = 9,
+            ["FooBar"] = 13,
+            ["Buzz"] = 17,
+            ["FooBaz"] = 99
+        };
+
+        // Act
+        IExtensibleDictionary<string, int> merged = first + second;
+
+        // Assert
+        merged.Should().BeSameAs(first);
+        merged.Should().BeEquivalentTo(new ExtendedDictionary<string, int>()
+        {
+            ["Foo"] = 5,
+            ["Bar"] = 8,
+            ["Baz"] = 13,
+            ["Fizz"] = 9,
+            ["FooBar"] = 13,
+            ["Buzz"] = 17,
+            ["FooBaz"] = 99
+        });
+    }
+
+    [Fact]
+    public void AddReverse()
+    {
+        // Arrange
+        IExtensibleDictionary<string, int> first = new ExtendedDictionary<string, int>()
+        {
+            ["Fizz"] = 9,
+            ["FooBar"] = 13,
+            ["Buzz"] = 17,
+            ["FooBaz"] = 99
+        };
+        IExtensibleDictionary<string, int> second = new ExtendedDictionary<string, int>()
+        {
+            ["Foo"] = 5,
+            ["Bar"] = 8,
+            ["Baz"] = 13
+        };
+
+        // Act
+        IExtensibleDictionary<string, int> merged = first + second;
+
+        // Assert
+        merged.Should().BeSameAs(first);
+        merged.Should().BeEquivalentTo(new ExtendedDictionary<string, int>()
+        {
+            ["Foo"] = 5,
+            ["Bar"] = 8,
+            ["Baz"] = 13,
+            ["Fizz"] = 9,
+            ["FooBar"] = 13,
+            ["Buzz"] = 17,
+            ["FooBaz"] = 99
+        });
+    }
+
+    [Fact]
+    public void Subtract()
+    {
+        // Arrange
+        IExtensibleDictionary<string, int> first = new ExtendedDictionary<string, int>()
+        {
+            ["Foo"] = 5,
+            ["Bar"] = 8,
+            ["Baz"] = 13
+        };
+        IExtensibleDictionary<string, int> second = new ExtendedDictionary<string, int>()
+        {
+            ["Fizz"] = 9,
+            ["Foo"] = 13,
+            ["Buzz"] = 17,
+            ["Baz"] = 13
+        };
+
+        // Act
+        IExtensibleDictionary<string, int> merged = first - second;
+
+        // Assert
+        merged.Should().BeSameAs(first);
+        merged.Should().BeEquivalentTo(new ExtendedDictionary<string, int>()
+        {
+            ["Foo"] = 5,
+            ["Bar"] = 8
+        });
+    }
+
+    [Fact]
+    public void SubtractReverse()
+    {
+        // Arrange
+        IExtensibleDictionary<string, int> first = new ExtendedDictionary<string, int>()
+        {
+            ["Fizz"] = 9,
+            ["Foo"] = 13,
+            ["Buzz"] = 17,
+            ["Baz"] = 13
+        };
+        IExtensibleDictionary<string, int> second = new ExtendedDictionary<string, int>()
+        {
+            ["Foo"] = 5,
+            ["Bar"] = 8,
+            ["Baz"] = 13
+        };
+
+        // Act
+        IExtensibleDictionary<string, int> merged = first - second;
+
+        // Assert
+        merged.Should().BeSameAs(first);
+        merged.Should().BeEquivalentTo(new ExtendedDictionary<string, int>()
+        {
+            ["Fizz"] = 9,
+            ["Foo"] = 13,
+            ["Buzz"] = 17
+        });
+    }
+}
