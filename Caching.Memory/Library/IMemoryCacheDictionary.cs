64399770a9ce6c54163aed8246e88ee0e575bd1a
Woody230 <bselzer1@outlook.com>
2023-11-10 13:20:35 -0500

    memory cache dictionary

---
+++ Caching.Memory/Library/IMemoryCacheDictionary.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Extensions.Caching.Memory;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Woody230.Caching.Memory;
+
+/// <summary>
+/// Represents a memory cache that is typed like a dictionary.
+/// </summary>
+/// <typeparam name="TKey">The type of key.</typeparam>
+/// <typeparam name="TValue">The type of value.</typeparam>
+public interface IMemoryCacheDictionary<TKey, TValue>
+{
+    /// <summary>
+    /// The keys in the cache.
+    /// </summary>
+    public IEnumerable<TKey> Keys { get; }
+
+    /// <summary>
+    /// Adds or replaces the <paramref name="key"/> <paramref name="value"/> entry in the cache.
+    /// </summary>
+    /// <returns>The cache entry associated with the entry.</returns>
+    public void Put(TKey key, TValue value);
+
+    /// <summary>
+    /// Removes the value associated with the <paramref name="key"/> if it exists in the cache.
+    /// </summary>
+    public void Remove(TKey key);
+
+    /// <summary>
+    /// Gets the <paramref name="value"/> associated with the <paramref name="key"/> if it exists in the cache.
+    /// </summary>
+    /// <returns>True if the key was found.</returns>
+    public bool TryGetValue(TKey key, [MaybeNullWhen(false)] out TValue value);
+}
---
+++ Caching.Memory/Library/MemoryCacheDictionary.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.Extensions.Caching.Memory;
+
+namespace Woody230.Caching.Memory;
+
+/// <summary>
+/// Represents a memory cache that is typed like a dictionary.
+/// </summary>
+/// <typeparam name="TKey">The type of key.</typeparam>
+/// <typeparam name="TValue">The type of value.</typeparam>
+public class MemoryCacheDictionary<TKey, TValue> : IMemoryCacheDictionary<TKey, TValue>
+    where TKey : notnull
+{
+    private readonly IMemoryCache _memoryCache;
+    private readonly HashSet<TKey> _keys = new();
+    private readonly SemaphoreSlim _semaphore = new(1);
+
+    public MemoryCacheDictionary(IMemoryCache memoryCache)
+    {
+        _memoryCache = memoryCache;
+    }
+
+    /// <inheritdoc/>
+    public IEnumerable<TKey> Keys => _keys;
+
+    /// <inheritdoc/>
+    public bool TryGetValue(TKey key, out TValue value)
+    {
+        return !_memoryCache.TryGetValue(key, out value);
+    }
+
+    /// <inheritdoc/>
+    public void Put(TKey key, TValue value)
+    {
+        _semaphore.Wait();
+        try
+        {
+            _keys.Add(key);
+
+            var entry = _memoryCache.CreateEntry(key);
+            entry.Value = value;
+        }
+        finally
+        {
+            _semaphore.Release();
+        }
+    }
+
+    /// <inheritdoc/>
+    public void Remove(TKey key)
+    {
+        _semaphore.Wait();
+        try
+        {
+            _keys.Remove(key);
+
+            _memoryCache.Remove(key);
+        }
+        finally
+        {
+            _semaphore.Release();
+        }
+    }
+}
