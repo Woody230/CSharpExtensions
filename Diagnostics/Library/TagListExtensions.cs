5237c1cc81344693ab69318f62b6fadff66ea3a9
Brandon Selzer <bselzer1@outlook.com>
2024-04-06 13:49:33 -0400

    tag list extensions

---
+++ Diagnostics/Library/TagListExtensions.cs
@@ -0,0 +1,41 @@
+﻿using System.Diagnostics;
+
+namespace Woody230.Diagnostics;
+
+/// <summary>
+/// Represents extensions for a <see cref="TagList"/>
+/// </summary>
+public static class TagListExtensions
+{
+    public static TagList With(this TagList @this, string key, object? value)
+    {
+        @this.Add(key, value);
+        return @this;
+    }
+
+    public static TagList With(this TagList @this, KeyValuePair<string, object?> tag)
+    {
+        @this.Add(tag);
+        return @this;
+    }
+
+    public static TagList With(this TagList @this, params KeyValuePair<string, object?>[] tags)
+    {
+        foreach (var tag in tags)
+        {
+            @this.Add(tag);
+        }
+
+        return @this;
+    }
+
+    public static TagList With(this TagList @this, in TagList tags)
+    {
+        foreach (var tag in tags)
+        {
+            @this.Add(tag);
+        }
+
+        return @this;
+    }
+}
