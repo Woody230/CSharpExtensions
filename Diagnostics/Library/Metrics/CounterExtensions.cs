89b18a7fc034f28ff0d7c64c1c03e24666f8a8d3
Brandon Selzer <bselzer1@outlook.com>
2024-04-05 22:42:43 -0400

    addMs counter extensions

---
+++ Diagnostics/Library/Metrics/CounterExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System.Diagnostics;
+using System.Diagnostics.Metrics;
+
+namespace Woody230.Diagnostics.Metrics;
+
+/// <summary>
+/// Represents extensions for a <see cref="Counter{T}"/>
+/// </summary>
+public static class CounterExtensions
+{
+    public static void AddMs(this Counter<double> counter, TimedResult result)
+    {
+        counter.Add(result.Time.TotalMilliseconds);
+    }
+
+    public static void AddMs(this Counter<double> counter, TimedResult result, TagList tags)
+    {
+        counter.Add(result.Time.TotalMilliseconds, tags);
+    }
+
+    public static void AddMs(this Counter<double> counter, TimedResult result, params KeyValuePair<string, object?>[] tags)
+    {
+        counter.Add(result.Time.TotalMilliseconds, tags);
+    }
+}
