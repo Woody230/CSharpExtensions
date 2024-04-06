8267298298caa9b67a9ae63769bd396dacb7b18d
Brandon Selzer <bselzer1@outlook.com>
2024-04-05 22:27:06 -0400

    timed delegate

---
+++ Diagnostics/Library/TimedDelegate.cs
@@ -0,0 +1,49 @@
+﻿using System.Diagnostics;
+
+namespace Woody230.Diagnostics;
+
+/// <summary>
+/// Measures the time delegates take to execute.
+/// </summary>
+public class TimedDelegate
+{
+    public static readonly TimedDelegate Default = new();
+
+    private readonly Stopwatch _stopwatch;
+
+    public TimedDelegate(Stopwatch stopwatch)
+    {
+        _stopwatch = stopwatch;
+    }
+
+    public TimedDelegate(): this(new Stopwatch())
+    {
+    }
+
+    /// <summary>
+    /// Measures the time that the <paramref name="action"/> takes to execute.
+    /// </summary>
+    public TimedResult Measure(Action action)
+    {
+        _stopwatch.Restart();
+
+        action();
+
+        _stopwatch.Stop();
+
+        return new(_stopwatch.Elapsed);
+    }
+
+    /// <summary>
+    /// Measures the time that the <paramref name="function"/> takes to execute.
+    /// </summary>
+    public TimedResult<T> Measure<T>(Func<T> function)
+    {
+        _stopwatch.Restart();
+
+        var result = function();
+        _stopwatch.Stop();
+
+        return new(_stopwatch.Elapsed, result);
+    }
+}
---
+++ Diagnostics/Library/TimedResult.cs
@@ -0,0 +1,22 @@
+﻿namespace Woody230.Diagnostics;
+
+/// <summary>
+/// Represents how long a delegate took to execute.
+/// </summary>
+/// <param name="Time">How long the delegate took to execute.</param>
+public record TimedResult(TimeSpan Time)
+{
+    public static implicit operator TimeSpan(TimedResult result) => result.Time;
+}
+
+/// <summary>
+/// Represents the result of a delegate and how long it took to execute.
+/// </summary>
+/// <typeparam name="T">The type of value.</typeparam>
+/// <param name="Value">The result of the delegate.</param>
+/// <param name="Time">How long the delegate took to execute.</param>
+public record TimedResult<T>(TimeSpan Time, T Value) : TimedResult(Time)
+{
+    public static implicit operator TimeSpan(TimedResult<T> result) => result.Time;
+    public static implicit operator T(TimedResult<T> result) => result.Value;
+}
