7f4fa546acc6ef6f3694a7ed71d76b144e24eb27
Brandon Selzer <bselzer1@outlook.com>
2024-04-07 08:32:39 -0400

    occurrence instrument

--- Diagnostics/Library/Metrics/MeterExtensions.cs
+++ Diagnostics/Library/Metrics/MeterExtensions.cs
@@ -7,6 +7,7 @@ namespace Woody230.Diagnostics.Metrics;
 /// </summary>
 public static class MeterExtensions
 {
+    #region Standard Instruments
     public static Counter<T> CreateCounter<T>(this Meter meter, InstrumentOptions options) where T: struct
     {
 #if NET6_0
@@ -100,36 +101,49 @@ public static class MeterExtensions
         return meter.CreateUpDownCounter<T>(options.Name, options.Unit, options.Description, options.Tags);
     }
 #endif
+    #endregion Standard Instruments
 
+    #region TimeSpanCounter
     public static TimeSpanCounter CreateTimeSpanCounter(this Meter meter, InstrumentOptions options, TimeInterval interval)
     {
-        var counter = meter.CreateCounter<double>(options);
-        return new TimeSpanCounter(counter, interval);
+        return interval switch
+        {
+            TimeInterval.Days => CreateDayCounter(meter, options),
+            TimeInterval.Hours => CreateHourCounter(meter, options),
+            TimeInterval.Minutes => CreateMinuteCounter(meter, options),
+            TimeInterval.Seconds => CreateSecondCounter(meter, options),
+            TimeInterval.Milliseconds => CreateMillisecondCounter(meter, options),
+#if NET8_0_OR_GREATER
+            TimeInterval.Microsceonds => CreateMicrosecondCounter(meter, options),
+            TimeInterval.Nanoseconds => CreateNanosecondCounter(meter, options)
+#endif
+            _ => CreateMillisecondCounter(meter, options),
+        };
     }
 
     public static TimeSpanCounter CreateDayCounter(this Meter meter, InstrumentOptions options)
     {
-        return CreateTimeSpanCounter(meter, options with { Unit = InstrumentUnit.Day }, TimeInterval.Days);
+        return InternalCreateTimeSpanCounter(meter, options, InstrumentUnit.Day, TimeInterval.Days);
     }
 
     public static TimeSpanCounter CreateHourCounter(this Meter meter, InstrumentOptions options)
     {
-        return CreateTimeSpanCounter(meter, options with { Unit = InstrumentUnit.Hour }, TimeInterval.Hours);
+        return InternalCreateTimeSpanCounter(meter, options, InstrumentUnit.Hour, TimeInterval.Hours);
     }
 
     public static TimeSpanCounter CreateMinuteCounter(this Meter meter, InstrumentOptions options)
     {
-        return CreateTimeSpanCounter(meter, options with { Unit = InstrumentUnit.Minute }, TimeInterval.Minutes);
+        return InternalCreateTimeSpanCounter(meter, options, InstrumentUnit.Minute, TimeInterval.Minutes);
     }
 
     public static TimeSpanCounter CreateSecondCounter(this Meter meter, InstrumentOptions options)
     {
-        return CreateTimeSpanCounter(meter, options with { Unit = InstrumentUnit.Second }, TimeInterval.Seconds);
+        return InternalCreateTimeSpanCounter(meter, options, InstrumentUnit.Second, TimeInterval.Seconds);
     }
 
     public static TimeSpanCounter CreateMillisecondCounter(this Meter meter, InstrumentOptions options)
     {
-        return CreateTimeSpanCounter(meter, options with { Unit = InstrumentUnit.Millisecond }, TimeInterval.Milliseconds);
+        return InternalCreateTimeSpanCounter(meter, options, InstrumentUnit.Millisecond, TimeInterval.Milliseconds);
     }
 
 #if NET8_0_OR_GREATER
@@ -143,4 +157,20 @@ public static class MeterExtensions
         return CreateTimeSpanCounter(meter, options with { Unit = InstrumentUnit.Nanosecond }, TimeInterval.Nanoseconds);
     }
 #endif
+
+    internal static TimeSpanCounter InternalCreateTimeSpanCounter(this Meter meter, InstrumentOptions options, InstrumentUnit unit, TimeInterval interval)
+    {
+        var counter = meter.CreateCounter<double>(options with { Unit = options.Unit ?? unit });
+        return new TimeSpanCounter(counter, interval);
+    }
+    #endregion TimeSpanCounter
+
+    #region OccurrenceInstrument
+    public static OccurrenceInstrument CreateOccurrenceInstrument(this Meter meter, InstrumentOptions options, TimeInterval interval)
+    {
+        var occurrenceCounter = meter.CreateCounter<long>(options);
+        var timeCounter = meter.CreateTimeSpanCounter(options with { Unit = null }, interval);
+        return new(meter, occurrenceCounter, timeCounter);
+    }
+    #endregion
 }
---
+++ Diagnostics/Library/Metrics/OccurrenceInstrument.cs
@@ -0,0 +1,81 @@
+﻿using System.Diagnostics;
+using System.Diagnostics.Metrics;
+
+namespace Woody230.Diagnostics.Metrics;
+
+/// <summary>
+/// Represents an instrument that measures the occurrence of an event.
+/// </summary>
+public class OccurrenceInstrument : Instrument
+{
+    private readonly Counter<long> _occurrenceCounter;
+    private readonly TimeSpanCounter _timeCounter;
+
+    internal OccurrenceInstrument(Meter meter, Counter<long> occurrenceCounter, TimeSpanCounter timeCounter) : base(meter, occurrenceCounter.Name, occurrenceCounter.Unit, occurrenceCounter.Description)
+    {
+        _occurrenceCounter = occurrenceCounter;
+        _timeCounter = timeCounter;
+    }
+
+    public void Record(Action action)
+    {
+        var result = new Stopwatch().Measure(action);
+        _occurrenceCounter.Add();
+        _timeCounter.Add(result);
+    }
+
+    public void Record(Action action, in TagList tags)
+    {
+        var result = new Stopwatch().Measure(action);
+        _occurrenceCounter.Add(tags);
+        _timeCounter.Add(result, tags);
+    }
+
+    public void Record(Action action, params KeyValuePair<string, object?>[] tags)
+    {
+        var result = new Stopwatch().Measure(action);
+        _occurrenceCounter.Add(tags);
+        _timeCounter.Add(result, tags);
+    }
+
+    public void Record(Action action, Func<TagList> getTags)
+    {
+        var result = new Stopwatch().Measure(action);
+        var tags = getTags();
+        _occurrenceCounter.Add(tags);
+        _timeCounter.Add(result, tags);
+    }
+
+    public T Record<T>(Func<T> function)
+    {
+        var result = new Stopwatch().Measure(function);
+        _occurrenceCounter.Add();
+        _timeCounter.Add(result);
+        return result;
+    }
+
+    public T Record<T>(Func<T> function, in TagList tags)
+    {
+        var result = new Stopwatch().Measure(function);
+        _occurrenceCounter.Add(tags);
+        _timeCounter.Add(result, tags);
+        return result;
+    }
+
+    public T Record<T>(Func<T> function, params KeyValuePair<string, object?>[] tags)
+    {
+        var result = new Stopwatch().Measure(function);
+        _occurrenceCounter.Add(tags);
+        _timeCounter.Add(result, tags);
+        return result;
+    }
+
+    public T Record<T>(Func<T> function, Func<T, TagList> getTags)
+    {
+        var result = new Stopwatch().Measure(function);
+        var tags = getTags(result);
+        _occurrenceCounter.Add(tags);
+        _timeCounter.Add(result, tags);
+        return result;
+    }
+}
