2b893b03a1accddfd27ed0053c1072b3dadfc9ee
Brandon Selzer <bselzer1@outlook.com>
2024-04-07 07:41:20 -0400

    stopwatch extensions

---
+++ Diagnostics/Library/StopwatchExtensions.cs
@@ -0,0 +1,33 @@
+﻿using System.Diagnostics;
+
+namespace Woody230.Diagnostics;
+
+/// <summary>
+/// Represents extensions for a <see cref="Stopwatch"/>.
+/// </summary>
+public static class StopwatchExtensions
+{
+    /// <summary>
+    /// Measures the time that the <paramref name="action"/> takes to execute.
+    /// </summary>
+    public static TimedResult Measure(this Stopwatch stopwatch, Action action)
+    {
+        stopwatch.Restart();
+        action();
+        stopwatch.Stop();
+
+        return new(stopwatch.Elapsed);
+    }
+
+    /// <summary>
+    /// Measures the time that the <paramref name="function"/> takes to execute.
+    /// </summary>
+    public static TimedResult<T> Measure<T>(this Stopwatch stopwatch, Func<T> function)
+    {
+        stopwatch.Restart();
+        var result = function();
+        stopwatch.Stop();
+
+        return new(stopwatch.Elapsed, result);
+    }
+}
