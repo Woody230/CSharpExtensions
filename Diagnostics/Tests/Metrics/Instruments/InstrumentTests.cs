7a7087d15241b8aabbb598c5e9c385130e40ee22
Brandon Selzer <bselzer1@outlook.com>
2024-04-07 12:36:25 -0400

    time span counter tests

---
+++ Diagnostics/Tests/Metrics/Instruments/InstrumentTests.cs
@@ -0,0 +1,22 @@
+﻿using System.Diagnostics;
+using System.Diagnostics.Metrics;
+using Woody230.Diagnostics.Metrics;
+
+namespace Woody230.Diagnostics.Tests.Metrics.Instruments;
+public abstract class InstrumentTests
+{
+    protected Meter Meter { get; } = new("woody230.metrics");
+    protected Guid Id { get; } = new("2f3c8e5e-8f3f-4daa-941c-80d390684d5a");
+    protected TagList Tags { get; }
+    protected InstrumentOptions Options { get; }
+
+    public InstrumentTests()
+    {
+        Tags = new TagList().With("woody230.id", Id);
+        Options = new("woody230.entity")
+        {
+            Description = "processing entity",
+            Tags = Tags,
+        };
+    }
+}
---
+++ Diagnostics/Tests/Metrics/Instruments/TimeSpanCounterTests.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.Extensions.Diagnostics.Metrics.Testing;
+using System.Diagnostics;
+using Woody230.Diagnostics.Metrics.Instruments;
+
+namespace Woody230.Diagnostics.Tests.Metrics.Instruments;
+
+/// <summary>
+/// Represents tests for a <see cref="TimeSpanCounter"/>
+/// </summary>
+public class TimeSpanCounterTests : InstrumentTests
+{
+    protected MetricCollector<double> Metrics { get; }
+
+    public TimeSpanCounterTests()
+    {
+        Metrics = new(Meter, Options.Name);
+    }
+
+    [Theory]
+    [MemberData(nameof(IntervalData))]
+    public void NoTags_RecordsTimeSpan(TimeInterval interval, TimeSpan elapsed, double value)
+    {
+        // Arrange
+        var counter = Meter.CreateTimeSpanCounter(Options, interval);
+
+        // Act
+        counter.Add(elapsed);
+
+        // Assert
+        var measurements = Metrics.GetMeasurementSnapshot();
+        measurements.Should().HaveCount(1);
+
+        var measurement = measurements[0];
+        measurement.Value.Should().Be(value);
+        measurement.Tags.Should().BeEmpty();
+    }
+
+    [Theory]
+    [MemberData(nameof(IntervalData))]
+    public void WithTagList_RecordsTimeSpan(TimeInterval interval, TimeSpan elapsed, double value)
+    {
+        // Arrange
+        var counter = Meter.CreateTimeSpanCounter(Options, interval);
+
+        // Act
+        counter.Add(elapsed, Tags);
+
+        // Assert
+        var measurements = Metrics.GetMeasurementSnapshot();
+        measurements.Should().HaveCount(1);
+
+        var measurement = measurements[0];
+        measurement.Value.Should().Be(value);
+        measurement.Tags.ToTagList().Should().BeEquivalentTo(Tags);
+    }
+
+    [Theory]
+    [MemberData(nameof(IntervalData))]
+    public void WithTagParams_RecordsTimeSpan(TimeInterval interval, TimeSpan elapsed, double value)
+    {
+        // Arrange
+        var counter = Meter.CreateTimeSpanCounter(Options, interval);
+
+        // Act
+        counter.Add(elapsed, KeyValuePair.Create<string, object>("type", nameof(TimeSpanCounterTests)));
+
+        // Assert
+        var measurements = Metrics.GetMeasurementSnapshot();
+        measurements.Should().HaveCount(1);
+
+        var measurement = measurements[0];
+        measurement.Value.Should().Be(value);
+        measurement.Tags.ToTagList().Should().BeEquivalentTo(new TagList().With("type", nameof(TimeSpanCounterTests)));
+    }
+
+
+    public static IEnumerable<object[]> IntervalData
+    {
+        get
+        {
+            yield return [
+                TimeInterval.Days,
+                new TimeSpan(2, 12, 0, 0, 0, 0),
+                2.5
+            ];
+        }
+    }
+}
