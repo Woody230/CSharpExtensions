e786b06060629f1b8bfeb3917d57bd1247b685bd
Brandon Selzer <bselzer1@outlook.com>
2024-04-08 19:05:35 -0400

    add docs

--- Diagnostics/Library/Metrics/Instruments/TimedOccurrenceCounter.cs
+++ Diagnostics/Library/Metrics/Instruments/TimedOccurrenceCounter.cs
@@ -19,21 +19,33 @@ public sealed class TimedOccurrenceCounter : OptionsInstrument
 
     public new bool Enabled => _occurrenceCounter.Enabled || _timeCounter.Enabled;
 
+    /// <summary>
+    /// Records a single occurrence and the amount of time the <paramref name="action"/> takes to execute.
+    /// </summary>
     public void Record(Action action)
     {
         Record(action, 1);
     }
 
+    /// <summary>
+    /// Records a single occurrence and the amount of time the <paramref name="action"/> takes to execute, with the given <paramref name="tags"/>.
+    /// </summary>
     public void Record(Action action, in TagList tags)
     {
         Record(action, 1, tags);
     }
 
+    /// <summary>
+    /// Records a single occurrence and the amount of time the <paramref name="action"/> takes to execute, with the tags from <paramref name="getTags"/>.
+    /// </summary>
     public void Record(Action action, Func<TagList> getTags)
     {
         Record(action, 1, getTags);
     }
 
+    /// <summary>
+    /// Records the number of <paramref name="occurrences"/> and the amount of time the <paramref name="action"/> takes to execute.
+    /// </summary>
     public void Record(Action action, long occurrences)
     {
         var result = new Stopwatch().Measure(action);
@@ -41,6 +53,9 @@ public sealed class TimedOccurrenceCounter : OptionsInstrument
         _timeCounter.Add(result);
     }
 
+    /// <summary>
+    /// Records the number of <paramref name="occurrences"/> and the amount of time the <paramref name="action"/> takes to execute, with the given <paramref name="tags"/>.
+    /// </summary>
     public void Record(Action action, long occurrences, in TagList tags)
     {
         var result = new Stopwatch().Measure(action);
@@ -48,6 +63,9 @@ public sealed class TimedOccurrenceCounter : OptionsInstrument
         _timeCounter.Add(result, tags);
     }
 
+    /// <summary>
+    /// Records the number of <paramref name="occurrences"/> and the amount of time the <paramref name="action"/> takes to execute, with the tags from <paramref name="getTags"/>.
+    /// </summary>
     public void Record(Action action, long occurrences, Func<TagList> getTags)
     {
         var result = new Stopwatch().Measure(action);
@@ -56,21 +74,37 @@ public sealed class TimedOccurrenceCounter : OptionsInstrument
         _timeCounter.Add(result, tags);
     }
 
+    /// <summary>
+    /// Records a single occurrence and the amount of time the <paramref name="function"/> takes to execute.
+    /// </summary>
+    /// <returns>The result of the <paramref name="function"/>.</returns>
     public T Record<T>(Func<T> function)
     {
         return Record(function, 1);
     }
 
+    /// <summary>
+    /// Records a single occurrence and the amount of time the <paramref name="function"/> takes to execute, with the given <paramref name="tags"/>.
+    /// </summary>
+    /// <returns>The result of the <paramref name="function"/>.</returns>
     public T Record<T>(Func<T> function, in TagList tags)
     {
         return Record(function, 1, tags);
     }
 
+    /// <summary>
+    /// Records a single occurrence and the amount of time the <paramref name="function"/> takes to execute, with the tags from <paramref name="getTags"/>.
+    /// </summary>
+    /// <returns>The result of the <paramref name="function"/>.</returns>
     public T Record<T>(Func<T> function, Func<T, TagList> getTags)
     {
         return Record(function, 1, getTags);
     }
 
+    /// <summary>
+    /// Records the number of <paramref name="occurrences"/> and the amount of time the <paramref name="function"/> takes to execute.
+    /// </summary>
+    /// <returns>The result of the <paramref name="function"/>.</returns>
     public T Record<T>(Func<T> function, long occurrences)
     {
         var result = new Stopwatch().Measure(function);
@@ -79,6 +113,10 @@ public sealed class TimedOccurrenceCounter : OptionsInstrument
         return result;
     }
 
+    /// <summary>
+    /// Records the number of <paramref name="occurrences"/> and the amount of time the <paramref name="function"/> takes to execute, with the given <paramref name="tags"/>.
+    /// </summary>
+    /// <returns>The result of the <paramref name="function"/>.</returns>
     public T Record<T>(Func<T> function, long occurrences, in TagList tags)
     {
         var result = new Stopwatch().Measure(function);
@@ -87,6 +125,10 @@ public sealed class TimedOccurrenceCounter : OptionsInstrument
         return result;
     }
 
+    /// <summary>
+    /// Records the number of <paramref name="occurrences"/> and the amount of time the <paramref name="function"/> takes to execute, with the tags from <paramref name="getTags"/>.
+    /// </summary>
+    /// <returns>The result of the <paramref name="function"/>.</returns>
     public T Record<T>(Func<T> function, long occurrences, Func<T, TagList> getTags)
     {
         var result = new Stopwatch().Measure(function);
