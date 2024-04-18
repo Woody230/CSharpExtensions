15a8081c2dadc4c852f67cdc21ca04961d76596e
Brandon Selzer <bselzer1@outlook.com>
2024-04-18 07:29:33 -0400

    convert to adaptable number

---
+++ Math.Operations/Library/AdaptableNumberExtensions.cs
@@ -0,0 +1,37 @@
+﻿namespace Woody230.Math.Operations;
+public static class AdaptableNumberExtensions
+{
+    public static AdaptableNumber ToAdaptableNumberOrZero(this object value) => value switch
+    {
+        int @int => @int,
+        long @long => @long,
+        double @double => @double,
+        decimal @decimal => @decimal,
+        string @string => ToAdaptableNumberOrZero(@string),
+        _ => new AdaptableNumber()
+    };
+
+    public static AdaptableNumber ToAdaptableNumberOrZero(this string value)
+    {
+        if (int.TryParse(value, out var @int))
+        {
+            return @int;
+        }
+        else if (long.TryParse(value, out var @long))
+        {
+            return @long;
+        }
+        else if (decimal.TryParse(value, out var @decimal))
+        {
+            return @decimal;
+        }
+        else if (double.TryParse(value, out var @double))
+        {
+            return @double;
+        }
+        else
+        {
+            return new AdaptableNumber();
+        }
+    }
+}
