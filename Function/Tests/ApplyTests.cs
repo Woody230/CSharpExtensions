52a694302b79f28828f3bc7e819e23f0f725e08e
Brandon Selzer <bselzer1@outlook.com>
2025-06-13 08:19:10 -0400

    run tests

--- Function/Tests/ApplyTests.cs
+++ Function/Tests/ApplyTests.cs
@@ -11,7 +11,7 @@ public class ApplyTests
         String = "FizzBuzz"
     };
 
-    private readonly TestObject _actionObject = new()
+    private readonly TestObject _blockObject = new()
     {
         Int = 5,
         Bool = true,
@@ -22,15 +22,15 @@ public class ApplyTests
     public void Apply_NoReturn_ReturnsObject()
     {
         // Arrange
-        void Action() => _actionObject.Int += 1;
+        void Block() => _blockObject.Int += 1;
 
         // Act
-        var result = _object.Apply(Action);
+        var result = _object.Apply(Block);
 
         // Assert
         result.Should().Be(_object);
 
-        _actionObject.Should().BeEquivalentTo(new TestObject()
+        _blockObject.Should().BeEquivalentTo(new TestObject()
         {
             Int = 6,
             Bool = true,
@@ -42,15 +42,15 @@ public class ApplyTests
     public void Apply_WithObject_ReturnsObject()
     {
         // Arrange
-        void Action(TestObject @object) => _actionObject.Int += @object.Int;
+        void Block(TestObject @object) => _blockObject.Int += @object.Int;
 
         // Act
-        var result = _object.Apply(Action);
+        var result = _object.Apply(Block);
 
         // Assert
         result.Should().Be(_object);
 
-        _actionObject.Should().BeEquivalentTo(new TestObject()
+        _blockObject.Should().BeEquivalentTo(new TestObject()
         {
             Int = 914824,
             Bool = true,
@@ -62,15 +62,15 @@ public class ApplyTests
     public void Apply_WithReturn_ReturnsObject()
     {
         // Arrange
-        int Action() => _actionObject.Int += 1;
+        int Block() => _blockObject.Int += 1;
 
         // Act
-        var result = _object.Apply(Action);
+        var result = _object.Apply(Block);
 
         // Assert
         result.Should().Be(_object);
 
-        _actionObject.Should().BeEquivalentTo(new TestObject()
+        _blockObject.Should().BeEquivalentTo(new TestObject()
         {
             Int = 6,
             Bool = true,
@@ -82,15 +82,15 @@ public class ApplyTests
     public void Apply_WithObject_WithReturn_ReturnsObject()
     {
         // Arrange
-        int Action(TestObject @object) => _actionObject.Int += @object.Int;
+        int Block(TestObject @object) => _blockObject.Int += @object.Int;
 
         // Act
-        var result = _object.Apply(Action);
+        var result = _object.Apply(Block);
 
         // Assert
         result.Should().Be(_object);
 
-        _actionObject.Should().BeEquivalentTo(new TestObject()
+        _blockObject.Should().BeEquivalentTo(new TestObject()
         {
             Int = 914824,
             Bool = true,
