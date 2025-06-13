0b24a1846e5baafb40b6acffbf3e134a1e33b9d0
Brandon Selzer <bselzer1@outlook.com>
2025-06-13 07:55:46 -0400

    scope function extensions

---
+++ Function/Library/README.md
@@ -0,0 +1,6 @@
+![](https://img.shields.io/github/license/Woody230/CSharpExtensions)
+[![](https://img.shields.io/nuget/v/Woody230.Function)](https://www.nuget.org/packages/Woody230.Function)
+
+# Function
+
+Custom extensions for functions.
---
+++ Function/Library/ScopeFunctionExtensions.cs
@@ -0,0 +1,47 @@
+﻿namespace Woody230.Function;
+
+/// <summary>
+/// Represents extensions mimicking scope functions from Kotlin.
+/// </summary>
+/// <seealso href="https://kotlinlang.org/docs/scope-functions.html"/>
+public static class ScopeFunctionExtensions
+{
+    /// <summary>
+    /// Invokes the <paramref name="action"/>.
+    /// </summary>
+    /// <returns>The <paramref name="object"/>.</returns>
+    public static T Apply<T>(this T @object, Action action)
+    {
+        action();
+        return @object;
+    }
+
+    /// <summary>
+    /// Invokes the <paramref name="action"/> using the <paramref name="object"/>.
+    /// </summary>
+    /// <returns>The <paramref name="object"/>.</returns>
+    public static T Apply<T>(this T @object, Action<T> action)
+    {
+        action(@object);
+        return @object;
+    }
+
+    /// <summary>
+    /// Invokes the <paramref name="function"/> using the <paramref name="object"/>.
+    /// </summary>
+    /// <returns>The <paramref name="object"/>.</returns>
+    public static T Apply<T, R>(this T @object, Func<T, R> function)
+    {
+        function(@object);
+        return @object;
+    }
+
+    /// <summary>
+    /// Invokes the <paramref name="function"/> using the <paramref name="object"/>
+    /// </summary>
+    /// <returns>The result of the <paramref name="function"/>.</returns>
+    public static R Run<T, R>(this T @object, Func<T, R> function)
+    {
+        return function(@object);
+    }
+}
