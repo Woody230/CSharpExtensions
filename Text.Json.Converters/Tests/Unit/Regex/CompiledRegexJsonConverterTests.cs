07ff67d8484f860df8804ac25ec3c740b3376bb1
Brandon Selzer <bselzer1@outlook.com>
2025-06-22 10:04:39 -0400

    regex converter tests

---
+++ Text.Json.Converters/Tests/Unit/Regex/CompiledRegexJsonConverterTests.cs
@@ -0,0 +1,109 @@
+﻿using System.Text.Json;
+using System.Text.RegularExpressions;
+using Woody230.Function;
+
+namespace Woody230.Text.Json.Converters.Tests.Unit.Regex;
+
+/// <summary>
+/// Represents tests for the <see cref="CompiledRegexJsonConverter"/>
+/// </summary>
+public class CompiledRegexJsonConverterTests
+{
+    private readonly JsonSerializerOptions _serializerOptions = new JsonSerializerOptions().Apply(options =>
+    {
+        options.WriteIndented = true;
+        options.Converters.Add(new CompiledRegexJsonConverter());
+    });
+
+    [Fact]
+    public void Read()
+    {
+        // Arrange
+        var testObject = new TestRegexObject()
+        {
+            Regex = new("^(1|3|7)(2|4|9)$", RegexOptions.Compiled)
+        };
+
+        const string json = $$"""
+        {
+          "Regex": "^(1|3|7)(2|4|9)$"
+        }
+        """;
+
+        // Act
+        var deserialized = JsonSerializer.Deserialize<TestRegexObject>(json, _serializerOptions);
+
+        // Assert
+        deserialized.Should().BeEquivalentTo(testObject);
+        deserialized.Regex.IsMatch("19").Should().BeTrue();
+        deserialized.Regex.IsMatch("47").Should().BeFalse();
+    }
+
+    [Fact]
+    public void ReadNull()
+    {
+        // Arrange
+        var testObject = new TestRegexObject()
+        {
+            Regex = null
+        };
+
+        const string json = $$"""
+        {
+          "Regex": null
+        }
+        """;
+
+        // Act
+        var deserialized = JsonSerializer.Deserialize<TestRegexObject>(json, _serializerOptions);
+
+        // Assert
+        deserialized.Should().BeEquivalentTo(testObject);
+    }
+
+    [Theory]
+    [InlineData(RegexOptions.Compiled)]
+    [InlineData(RegexOptions.None)]
+    public void Write(RegexOptions options)
+    {
+        // Arrange
+        var testObject = new TestRegexObject()
+        {
+            Regex = new("^(1|3|7)(2|4|9)$", options)
+        };
+
+        const string json = $$"""
+        {
+          "Regex": "^(1|3|7)(2|4|9)$"
+        }
+        """;
+
+        // Act
+        var deserialized = JsonSerializer.Serialize(testObject, _serializerOptions);
+
+        // Assert
+        deserialized.Should().Be(json);
+    }
+
+    [Fact]
+    public void WriteNull()
+    {
+        // Arrange
+        var testObject = new TestRegexObject()
+        {
+            Regex = null
+        };
+
+        const string json = $$"""
+        {
+          "Regex": null
+        }
+        """;
+
+        // Act
+        var deserialized = JsonSerializer.Serialize(testObject, _serializerOptions);
+
+        // Assert
+        deserialized.Should().Be(json);
+    }
+}
---
+++ Text.Json.Converters/Tests/Unit/Regex/TestRegexObject.cs
@@ -0,0 +1,5 @@
+﻿namespace Woody230.Text.Json.Converters.Tests.Unit.Regex;
+public record TestRegexObject()
+{
+    public System.Text.RegularExpressions.Regex Regex { get; init; }
+}
