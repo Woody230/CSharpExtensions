840ffe665a6375c9767bc42096f50ae49e2dd82b
Brandon Selzer <bselzer1@outlook.com>
2025-06-22 07:08:58 -0400

    grouped lookup

---
+++ Collections.Extensible/Library/Generic/Lookup/GroupingExtensions.cs
@@ -0,0 +1,15 @@
+﻿namespace Woody230.Collections.Extensible.Generic.Lookup;
+
+/// <summary>
+/// Represents extensions for a <see cref="IGrouping{TKey, TElement}"/>.
+/// </summary>
+public static class GroupingExtensions
+{
+    /// <summary>
+    /// Wraps the <paramref name="values"/> as a <see cref="IGrouping{TKey, TElement}"/> for <paramref name="key"/>.
+    /// </summary>
+    public static IGrouping<TKey, TValue> ToGrouping<TKey, TValue>(this IEnumerable<TValue> values, TKey key)
+    {
+        return new Grouping<TKey, TValue>(key, values);
+    }
+}
---
+++ Collections.Extensible/Library/Generic/Lookup/LookupExtensions.cs
@@ -0,0 +1,15 @@
+﻿namespace Woody230.Collections.Extensible.Generic;
+
+/// <summary>
+/// Represents extensions for a <see cref="ILookup{TKey, TValue}"/>.
+/// </summary>
+public static class LookupExtensions
+{
+    /// <summary>
+    /// Wraps the <paramref name="groupings"/> as a <see cref="ILookup{TKey, TElement}"/>.
+    /// </summary>
+    public static ILookup<TKey, TValue> ToLookup<TKey, TValue>(this IEnumerable<IGrouping<TKey, TValue>> groupings) where TKey : notnull
+    {
+        return new GroupedLookup<TKey, TValue>(groupings);
+    }
+}
