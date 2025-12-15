348a33283f994e9b000bee18e45dcb3d3b352ec8
Brandon Selzer <bselzer1@outlook.com>
2025-12-15 09:20:25 -0500

    Feature/json merge (#21)
    Merging of System.Text.Json nodes.

---
+++ Text.Json.Merge/Library/DeepEqualityComparer.cs
@@ -0,0 +1,18 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Text.Json.Nodes;
+
+namespace Woody230.Text.Json.Merge;
+
+/// <summary>
+/// Compares nodes using deep equality.
+/// </summary>
+internal sealed class DeepEqualityComparer : EqualityComparer<JsonNode?>
+{
+    public static readonly DeepEqualityComparer Instance = new();
+
+    /// <inheritdoc/>
+    public override bool Equals(JsonNode? x, JsonNode? y) => JsonNode.DeepEquals(x, y);
+
+    /// <inheritdoc/>
+    public override int GetHashCode([DisallowNull] JsonNode obj) => obj.ToJsonString().GetHashCode();
+}
---
+++ Text.Json.Merge/Library/JsonMergeOptions.cs
@@ -0,0 +1,22 @@
+﻿namespace Woody230.Text.Json.Merge;
+
+/// <summary>
+/// The options for merging JSON nodes.
+/// </summary>
+public sealed record JsonMergeOptions
+{
+    /// <summary>
+    /// A singleton instance of <see cref="JsonMergeOptions"/> with <see cref="ArrayMergeHandling.Merge"/> and <see cref="NullMergeHandling.Ignore"/>
+    /// </summary>
+    public static readonly JsonMergeOptions Default = new();
+
+    /// <summary>
+    /// How to merge arrays.
+    /// </summary>
+    public ArrayMergeHandling ArrayHandling { get; init; } = ArrayMergeHandling.Merge;
+
+    /// <summary>
+    /// How to merge null values.
+    /// </summary>
+    public NullMergeHandling NullHandling { get; init; } = NullMergeHandling.Ignore;
+}
