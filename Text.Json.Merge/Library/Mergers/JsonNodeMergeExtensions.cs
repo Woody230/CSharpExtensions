4225c89f13f52a790748f25ae3a0098ed98e66e4
Woody230 <bselzer1@outlook.com>
2025-12-15 08:08:20 -0500

    merge with options extensions

--- Text.Json.Merge/Library/Mergers/JsonNodeMergeExtensions.cs
+++ Text.Json.Merge/Library/Mergers/JsonNodeMergeExtensions.cs
@@ -1,5 +1,6 @@
 ﻿using System.Diagnostics.CodeAnalysis;
 using System.Text.Json.Nodes;
+using Woody230.Text.Json.Merge.Models;
 
 namespace Woody230.Text.Json.Merge.Mergers;
 
@@ -40,5 +41,42 @@ public static class JsonNodeMergeExtensions
     public static JsonArray Merge(this JsonArray @this, JsonArray other)
     {
         return JsonMerger.Default.Merge(@this, other);
+    }
+
+    /// <summary>
+    /// Combines the contents of the <paramref name="this"/> node with the <paramref name="other"/> node with custom merging options.
+    /// </summary>
+    /// <param name="this">The first node.</param>
+    /// <param name="other">The second node.</param>
+    /// <param name="options">The merging options.</param>
+    /// <returns>The combined node.</returns>
+    [return: NotNullIfNotNull(nameof(other))]
+    public static JsonNode? Merge(this JsonNode @this, JsonNode? other, JsonMergeOptions options)
+    {
+        return new JsonMerger(options).Merge(@this, other);
+    }
+
+    /// <summary>
+    /// Combines the contents of the <paramref name="this"/> object with the <paramref name="other"/> object with custom merging options.
+    /// </summary>
+    /// <param name="this">The first object.</param>
+    /// <param name="other">The second object.</param>
+    /// <param name="options">The merging options.</param>
+    /// <returns>The combined object.</returns>
+    public static JsonObject Merge(this JsonObject @this, JsonObject other, JsonMergeOptions options)
+    {
+        return new JsonMerger(options).Merge(@this, other);
+    }
+
+    /// <summary>
+    /// Combines the contents of the <paramref name="this"/> array with the <paramref name="other"/> array with custom merging options.
+    /// </summary>
+    /// <param name="this">The first array.</param>
+    /// <param name="other">The second array.</param>
+    /// <param name="options">The merging options.</param>
+    /// <returns>The combined array.</returns>
+    public static JsonArray Merge(this JsonArray @this, JsonArray other, JsonMergeOptions options)
+    {
+        return new JsonMerger(options).Merge(@this, other);
     }
 }
--- Text.Json.Merge/Library/README.md
+++ Text.Json.Merge/Library/README.md
@@ -12,3 +12,6 @@ Merging of JSON nodes from the System.Text.Json namespace.
 | JsonNode? Merge(this JsonNode @this, JsonNode? other) | Combines the contents of the this node with the other node. |
 | JsonObject Merge(this JsonObject @this, JsonObject other) | Combines the contents of the this object with the other object. |
 | JsonArray Merge(this JsonArray @this, JsonArray other) | Combines the contents of the this array with the other array. |
+| JsonNode? Merge(this JsonNode @this, JsonNode? other, JsonMergeOptions options) | Combines the contents of the this node with the other node with custom merging options. |
+| JsonObject Merge(this JsonObject @this, JsonObject other, JsonMergeOptions options) | Combines the contents of the this object with the other object with custom merging options. |
+| JsonArray Merge(this JsonArray @this, JsonArray other, JsonMergeOptions options) | Combines the contents of the this array with the other array with custom merging options. |
